e78f015e399b757e77ebdb0ca18723b33adab31f
Anshiya <anshiyathaman@gmail.com>
2026-07-21 10:53:53 +0530

    Add: permutation-string - submission-0

---
+++ Data Structures & Algorithms/permutation-string/submission-0.py
@@ -0,0 +1,32 @@
+class Solution:
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+        if len(s1) > len(s2):
+            return False
+
+        count1 = [0] * 26
+        count2 = [0] * 26
+
+        # Count characters of s1 and first window of s2
+        for i in range(len(s1)):
+            count1[ord(s1[i]) - ord('a')] += 1
+            count2[ord(s2[i]) - ord('a')] += 1
+
+        # Check first window
+        if count1 == count2:
+            return True
+
+        # Slide the window
+        left = 0
+        for right in range(len(s1), len(s2)):
+            # Add new character
+            count2[ord(s2[right]) - ord('a')] += 1
+
+            # Remove left character
+            count2[ord(s2[left]) - ord('a')] -= 1
+            left += 1
+
+            # Compare frequency arrays
+            if count1 == count2:
+                return True
+
+        return False
